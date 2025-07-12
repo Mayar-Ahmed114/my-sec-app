d6ea86141aa9ed0e5513a9064a5f3b749e84f68e
Mayar-Ahmed114 <mayarahmed1266870@gmail.com>
2025-07-12 20:19:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const { useCallback } = require("react")
+const request = require ("request")
+const forecast = require("./data1/forecast")
+const geocode = require("./data1/geocode")
+
+const country = process.argv[2]
+geocode(country , (error , data)=>{
+    console.log("Error : " ,error)
+    console.log("Data : " , data)
+forecast(data.latitude , data.longtitude , (error , data)=>{
+console.log("Error :" , error)
+console.log("Data : " , data)
+})
+})
